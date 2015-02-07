98c5c0036964a103e1e7d76740dc05a1f14f3058
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-02-07 22:49:34 +0100

    Initialized project and added Angular Material Design module

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('writePaintApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
